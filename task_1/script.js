bf7416e833245de185fc63a5f6fccc5f1a92e545
Radik <kaddafi88@gmail.com>
2019-07-23 20:25:48 +0500

    Изменено согласно примеру

--- task_1/script.js
+++ task_1/script.js
@@ -1,31 +1,21 @@
-'use strict';
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-  money : 0,
-  timeData : '',
-  expenses: {
-    "ответ на первый вопрос" : "ответ на второй вопрос"
-  },
-  optionalExpenses : {},
-  income : [],
-  savings : false
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
 
-let money = +prompt("Ваш бюджет на месяц?"),
-    time = prompt("Введите дату в формате YYYY-MM-DD");
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
-console.log(money + ' ' + time);
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-appData.money = money;
-appData.timeData = time;
-
-alert("Ваш бюджет " + appData.money / 12);
-
-// Сколько типов данных существует в JS?
-// 8 типов данных
-
-// Как вывести информацию в консоль?
-// console.log();
-
-// Какая функция операторов || и &&?
-// || (ИЛИ), && (И)
+alert(appData.budget / 30);
