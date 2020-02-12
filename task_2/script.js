4198225ba71c1468200e1c702651085fe4cc5b8b
Radik <kaddafi88@gmail.com>
2020-02-12 06:33:53 +0500

    Изменения в цикле

--- task_2/script.js
+++ task_2/script.js
@@ -21,7 +21,7 @@ for (let i = 0; i < 2; i++) {
 		appData.expenses[a] = b;
 	} else {
 		console.log("else");
-		i = 0;
+		i--;
 	}
 }
 
@@ -38,5 +38,49 @@ if (appData.moneyPerDay < 100) {
 } else {
 	console.log("Произошла ошибка");	
 }
+
+// Используем цикл WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+
+
+
+// Используем цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
+
 
 console.log(appData);
