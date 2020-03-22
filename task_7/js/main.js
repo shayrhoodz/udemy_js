a2cc9b8178d000431eb6da2668aec6bd64b645dd
Radik <kaddafi88@gmail.com>
2020-03-22 08:03:36 +0500

    Выполнено 7 задание

---
+++ task_7/js/main.js
@@ -0,0 +1,20 @@
+let startBtn = document.getElementById('start');
+let budget = document.querySelector('.budget-value');
+let daybudget = document.querySelector('.daybudget-value');
+let level = document.querySelector('.level-value');
+let expenses = document.querySelector('.expenses-value');
+let optionalexpenses = document.querySelector('.optionalexpenses-value');
+let income = document.querySelector('.incom-value');
+let monthsavings = document.querySelector('.monthsavings-value');
+let yearsavings = document.querySelector('.yearsavings-value');
+let expensesItems = document.querySelectorAll('.expenses-item');
+let expensesBtn = document.querySelector('.expenses-item-btn');
+let optionalExpensesBtn = document.querySelector('.optionalexpenses-item-btn');
+let countBudgetBtn = document.querySelector('.count-budget-btn');
+let optionalExpensesItems = document.querySelectorAll('.optionalexpenses-item');
+let chooseIncome = document.querySelector('.choose-income');
+let savings = document.querySelector('#savings');
+let chooseSum = document.querySelector('.choose-sum');
+let choosePercent = document.querySelector('.choose-percent');
+
+
